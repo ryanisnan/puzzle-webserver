5d49c05e8ef0ef6278bef92cda72659b708cf9d2
ryanisnan <ryanisnan@gmail.com>
2017-08-08 00:52:48 -0700

    read S3DIRECT_REGION from env variables and also update bucket name

--- server/server/settings.py
+++ server/server/settings.py
@@ -130,10 +130,10 @@ AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID', '')
 AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY', '')
 AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME', '')
 
-S3DIRECT_REGION = 'us-east-1'
+S3DIRECT_REGION = os.environ.get('S3DIRECT_REGION', '')
 
 S3DIRECT_DESTINATIONS = {
     'puzzles': {
-        'key': 'puzzles',
+        'key': 'puzzles-webserver',
     }
 }
